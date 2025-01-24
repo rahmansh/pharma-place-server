be93f520b3eb60deff28b8be8f83c061ea78e7d8
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-01-24 20:00:14 +0100

    feat: implement delete api from cart

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ async function run() {
             res.send(result);
         })
 
+        app.delete("/carts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
         app.get("/carts", async (req, res) => {
             const { email, medicineName } = req.query;
 
