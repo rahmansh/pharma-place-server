79bbaf541fde3053f9d804a33914c89b9cc01d0e
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-08 01:39:46 +0100

    feat: API endpoint for stripe payment intent

--- index.js
+++ index.js
@@ -272,6 +272,25 @@ async function run() {
         })
 
 
+        // payment
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: [
+                    "card"
+                ]
+            })
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        })
+
+
 
 
 
